b4e3c13dcdb20e526f153c2c1b4a32bfd5c0805e
livinginatent <48744077+livinginatent@users.noreply.github.com>
2023-01-23 22:27:23 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import Home from "./Pages/Home";
+import AdditionMain from "./Pages/AdditionMain";
+import MultiplicationMain from "./Pages/MultiplicationMain";
+import SubtractionMain from "./Pages/SubtractionMain";
+import DivisionMain from "./Pages/DivisionMain";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FreeOrChallenge from "./components/FreeOrChallenge";
+
+function App() {
+  return (
+    <>
+      <div  className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/addition-practice/" element={<AdditionMain />} />
+            <Route
+              path="/subtraction-practice/"
+              element={<SubtractionMain />}
+            />
+            <Route
+              path="/multiplication-practice/"
+              element={<MultiplicationMain />}
+            />
+            <Route path="/division-practice/" element={<DivisionMain />} />
+            <Route path="/login/" element={<Login />} />
+            <Route path="/register/" element={<Register />} />
+            <Route path="/prac-challenge/" element={<FreeOrChallenge />} />
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer />
+      </div>
+    </>
+  );
+}
+
+export default App;
